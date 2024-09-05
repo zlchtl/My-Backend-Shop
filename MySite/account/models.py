fa1d55b00e97f0a031dcbe45502ec58fc0f54087
BadRedCrab <ebengrad1@mail.ru>
2024-09-05 16:27:49 +0300

    Создать модель пользоваетля и подключить ее в settings и admin

--- MySite/account/models.py
+++ MySite/account/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class CustomUser(AbstractUser):
+
+    is_email_verified = models.BooleanField(default=False)
+    birth_date = models.DateField(default=date.today)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username

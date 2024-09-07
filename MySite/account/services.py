8c4f17cb923922f9ce02fef7ed57f26a3914cb44
BadRedCrab <ebengrad1@mail.ru>
2024-09-07 22:20:13 +0300

    Исправление импорта EMAIL_HOST_USER

--- MySite/account/services.py
+++ MySite/account/services.py
@@ -1,15 +1,15 @@
 from knox.models import AuthToken
 from celery import shared_task
 from django.core.mail import send_mail
-from decouple import config
+from django.conf import settings
 
 @shared_task
 def SendAsyncEmailService(recipient):
     message = 'test'
-    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    your_mail = settings.EMAIL_HOST_USER
     try:
         send_mail('MySite подтверждение почты', message,
-                  EMAIL_HOST_USER, [recipient])
+                  your_mail, [recipient])
     except Exception as e:
         print(f"Error sending email: {str(e)}")
 

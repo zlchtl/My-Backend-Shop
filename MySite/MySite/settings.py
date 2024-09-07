b0a14296d626ae6701cde17ebf35ce09a7e8631a
BadRedCrab <ebengrad1@mail.ru>
2024-09-07 18:37:13 +0300

    Подключение Celery + Redis и их настройка

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -115,6 +115,12 @@ KNOX = {
     'TOKEN_TTL': timedelta(days=1),
 }
 
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 

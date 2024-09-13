fd14d564efc06d2a95430501a630a93f7f01ad35
BadRedCrab <ebengrad1@mail.ru>
2024-09-13 19:45:17 +0300

    Подключить django к postgresql

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -93,9 +93,13 @@ WSGI_APPLICATION = 'MySite.wsgi.application'
 # Database settings
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+    }
 }
 
 # Password validation
